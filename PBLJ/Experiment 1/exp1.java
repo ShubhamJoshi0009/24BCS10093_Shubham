af01d397ae3d955fc63dd6d59ee9ff8e8759917b
Shubham Joshi <shubham0009joshi@gmail.com>
2026-08-22 09:56:10 +0530

    Create exp1.java

---
+++ PBLJ/Experiment 1/exp1.java
@@ -0,0 +1,9 @@
+public class exp1 {
+    public static void main(String[] args){
+        String a="shubh";                 
+        String b=a;                          
+        a="got it";                  
+        String c="new";                 
+        String d=new String("mark me");
+    }
+}
