dd0966b75c2b1d2de13989359f9fbaf87251cd82
Shubham Joshi <shubham0009joshi@gmail.com>
2026-08-22 09:59:30 +0530

    Delete PBLJ/Experiment 1 directory

--- PBLJ/Experiment 1/exp1.java
+++
@@ -1,9 +0,0 @@
-public class exp1 {
-    public static void main(String[] args){
-        String a="shubh";                 
-        String b=a;                          
-        a="got it";                  
-        String c="new";                 
-        String d=new String("mark me");
-    }
-}
